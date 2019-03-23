1b9a89123a4c7d560a4b8e4327e7a3a56bb14ea6
Robert <gusroberth@student.gu.se>
2019-03-23 21:05:19 +0100

    up

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def fail():
 
 @app.route('/test')
 def test():
-    vxml = render_template('test.xml', song=song[0])
+    vxml = render_template('test.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response    
