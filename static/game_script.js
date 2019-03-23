2f0cc99fdfe2be220f131180016b7275eeb60aa1
Robert <gusroberth@student.gu.se>
2019-03-23 17:27:00 +0100

    up

--- static/game_script.js
+++ static/game_script.js
@@ -1,18 +1,35 @@
 var name = [];
+var score = 0;
+var tries = 3;
+var lives = 10;
 
-    function add_letter(letter){
-      name.push(letter);
-      return name
-      
-    }
+function add_letter(letter){
+  name.push(letter);
+  return name
+  
+}
 
-    function read_name(){
-      name = name.join('');
-      return name.toString()
+function read_name(){
+  name = name.join('');
+  return name.toString()
 
-  }
+}
 
-  function reset_name(name){
-    name = [];
-    return name
-  }
+function reset_name(name){
+  name = [];
+  return name
+}
+
+function reset_tries(tries){
+  tries = 3
+  return tries
+}
+
+function add_point(score) {
+    return score + 1;
+
+}
+
+function minus_point(score) {
+    return score - 1;
+}
