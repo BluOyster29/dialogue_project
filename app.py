245c160d1d9b3b680f8f1a7edc5a56bc3d69daf0
Robert <gusroberth@student.gu.se>
2019-03-23 19:44:28 +0100

    up

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, make_response, send_from_directory
 
 app = Flask(__name__)
 
-song = '/audio/metal.mp3'
+song = "audio/metal.wav"
 
 @app.route('/main_menu')
 def main_menu():
