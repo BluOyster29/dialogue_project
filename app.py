223cf705b2ca4454098cc8f7174033f67b870c7e
Robert <gusroberth@student.gu.se>
2019-03-23 18:57:55 +0100

    up

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, make_response, send_from_directory
 app = Flask(__name__)
 
-@app.route('/altered_beast')
-def game():
-    vxml = render_template('altered_beast.xml')
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
+audio = ['metal.wav']
 
 @app.route('/main_menu')
 def main_menu():
@@ -15,12 +10,6 @@ def main_menu():
     response.headers["Content-Type"] = "application/xml"
     return response
 
-@app.route('/level_one')
-def easy():
-    vxml = render_template('level_one.xml')
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
 
 @app.route('/fail')
 def fail():
@@ -36,32 +25,13 @@ def test():
     response.headers["Content-Type"] = "application/xml"
     return response    
 
-@app.route('/level_two')
-def medium():
-    vxml = render_template('level_two.xml')
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
-
 @app.route('/grammars/<path:path>')
 def send_grammar(path):
     return send_from_directory('grammars', path)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/level_one")
-def level_one():
-    return render_template("level_one.html")
-
-@app.route("/level_two")
-def level_two():
-    return render_template("level_two.html")
-
-@app.route("/level_three")
-def level_three():
-    return render_template("level_three.html")
+@app.route('/audio/<path:path>')
+def send_audio(path):
+    return send_from_directory('audio', path)
 
 if __name__ == "__main__":
     app.run(debug=True)
